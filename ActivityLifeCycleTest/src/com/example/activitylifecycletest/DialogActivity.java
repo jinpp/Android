7301866426daa8a00085b7527114e360eecd1e37
金盼盼 <jinpp>
2016-11-15 10:39:03 +0800

    android

---
+++ ActivityLifeCycleTest/src/com/example/activitylifecycletest/DialogActivity.java
@@ -0,0 +1,17 @@
+package com.example.activitylifecycletest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class DialogActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.dialog_layout);
+	}
+
+}
